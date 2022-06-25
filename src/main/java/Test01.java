3136023bd4f88a1f11617fb566027886612ca83a
dawn24k <dawn24k@163.com>
2022-06-25 11:58:30 +0800

    测试commit3

---
+++ src/main/java/Test01.java
@@ -0,0 +1,11 @@
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("12348565");
+    }
+
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
