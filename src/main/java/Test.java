cb8e06c9662b76fb738b0b0b8a048eac7b9f2080
dawn24k <dawn24k@163.com>
2022-06-25 11:43:46 +0800

    测试commit2

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,4 +2,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("123");
     }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
